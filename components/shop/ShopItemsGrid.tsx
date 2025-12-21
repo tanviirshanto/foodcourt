e801b1898320b353f8d7db12080fadcb1a2017c3
TANVIR HOSSEN SHANTO <tanviir.hossen@gmail.com>
2025-12-21 13:55:41 +0600

    fix: Can't resolve '@/components/Shop/ShopItemsGrid'

--- components/shop/ShopItemsGrid.tsx
+++ components/shop/ShopItemsGrid.tsx
@@ -1,15 +1,15 @@
-import { ShopItem } from "@/types/recommended";
 import ShopItemCard from "@/components/Shop/ShopItemCard";
+import type { ItemType } from "@/types/shop";
 
 interface ShopItemsGridProps {
-  items: ShopItem[];
+  items: ItemType[];
 }
 
 export default function ShopItemsGrid({ items }: ShopItemsGridProps) {
   return (
     <section className="max-w-7xl mx-auto px-6 md:px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mb-24">
-      {items?.map((item) => (
-        <ShopItemCard key={item._id} item={item} />
+      {items.map((item) => (
+        <ShopItemCard key={String(item._id)} item={item as any} />
       ))}
     </section>
   );
